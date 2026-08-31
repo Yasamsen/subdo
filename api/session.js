fb79272a0b52fe20d1131b960f6b80deca4323db
ALyaSam <162271637+Yasamsen@users.noreply.github.com>
2026-08-31 22:02:51 +0800

    Update api/session.js

--- api/session.js
+++ api/session.js
@@ -1,2 +1,4 @@
 const { verifySession } = require("./_auth");
-module.exports=async function(req,res){ res.json({authenticated:verifySession(req)}); };
+
+module.exports = async function (req, res) {
+  res.setHeader("Cache-Control", "no-store"
